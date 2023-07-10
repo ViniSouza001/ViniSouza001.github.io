80e804ec4130d2f66161ea5fb972e8d3858ddd35
ViniSouza001 <viniciussouza130705@gmail.com>
2023-07-10 15:07:14 -0300

    reorganization

--- scripts/home.js
+++ scripts/home.js
@@ -1,10 +1,1 @@
-function entrar() {
-    setTimeout(() => {
-        window.location.href = './page2/portfolio.html'
-    }, 1000);
-}
-
 const main = document.querySelector('main');
-function transicao() {
-    main.classList.add('fadeout');
-}
