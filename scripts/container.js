1312170ae4c57162526c531644e9c849ddbc8ccf
ViniSouza001 <viniciussouza130705@gmail.com>
2023-11-22 07:57:08 -0300

    portfolio

---
+++ scripts/container.js
@@ -0,0 +1,49 @@
+const videos = [
+    {
+        "titulo": "Fisolofando",
+        "imagem": "../assets/fisolofando.png",
+        "descricao": "A short video that I made in a school assignment",
+        "link": "https://youtu.be/tbiGlqCiYYk"
+    },
+    {
+        "titulo": "Faded",
+        "imagem": "../assets/faded.png",
+        "descricao": "An edition I made of Alan Waker's song Faded",
+        "link": "https://youtu.be/HuhpTcpFNYQ"
+    },
+    {
+        "titulo": "Just the way you are",
+        "imagem": "../assets/bruno_mars.png",
+        "descricao": "An edition I made of Bruno Mars' song Just the way you are",
+        "link": "https://youtu.be/bAJqpubWuAA"
+    },
+    {
+        "titulo": "Photograph",
+        "imagem": "../assets/photograph.png",
+        "descricao": "An edition I made of Ed Sheeran's song Photograph",
+        "link": "https://youtu.be/TkgEAd2W0V4"
+    },
+    {
+        "titulo": "Blinding Lights",
+        "imagem": "../assets/blinding_lights.png",
+        "descricao": "An edition I made of The Weeknd's song Blinding Lights",
+        "link": "https://youtu.be/gWDu1GXAgrU"
+    }
+]
+
+const projetos = [
+    {
+        "titulo": "Fisolofando.com",
+        "imagem": "../assets/fisolofando_site.png",
+        "descricao": "A school assignment that I made with the video",
+        "linkProj": "https://fisolofando.netlify.app/",
+        "linkGit": "https://github.com/ViniSouza001/fisolofando"
+    },
+    {
+        "titulo": "BlogApp",
+        "imagem": "../assets/blogapp_inicial_screen.png",
+        "descricao": "A project created studying Node.js",
+        "linkGit": "https://github.com/ViniSouza001/BlogApp/",
+        "linkProj": "https://blogapp-proj.vercel.app/"
+    }
+]
